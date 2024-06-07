129deab1ad32bf26606e40419bb2dca35baba1a2
Dylan Delgado <dylandts03@gmail.com>
2024-06-06 20:08:04 -0500

    Initial Commit

---
+++ SurveyApp/SurveyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyAppApp.swift
+//  SurveyApp
+//
+//  Created by Dylan Delgado on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SurveyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
